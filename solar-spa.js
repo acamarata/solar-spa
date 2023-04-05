a4b07f8cee575a629334b84487eacafae34793e8
Ali Camarata <alisalaah@gmail.com>
2023-04-04 21:16:18 -0400

    Revert bug fix in 1.2.1

--- solar-spa.js
+++ solar-spa.js
@@ -1,4 +1,5 @@
-const spaModuleFactory = require('./spa.js');
+// solar-spa.js
+const spaModule = require('./spa.js');
 
 module.exports = function spa(
   date,
@@ -9,58 +10,53 @@ module.exports = function spa(
   pressure = 1013.25,
   refraction = 0.5667
 ) {
-  return new Promise((resolve, reject) => {
-    const spaModule = spaModuleFactory({
-      onRuntimeInitialized: () => {
-        const spa_calculate = spaModule.cwrap(
-          'spa_calculate_wrapper',
-          'number',
-          [
-            'number', 'number', 'number', 'number', 'number',
-            'number', 'number', 'number', 'number', 'number',
-            'number', 'number', 'number', 'number', 'number'
-          ]
-        );
+  return new Promise((resolve) => {
+    spaModule.onRuntimeInitialized = function () {
+      const spa_calculate = spaModule.cwrap(
+        'spa_calculate_wrapper',
+        'number',
+        [
+          'number', 'number', 'number', 'number', 'number',
+          'number', 'number', 'number', 'number', 'number',
+          'number', 'number', 'number', 'number', 'number'
+        ]
+      );
 
-        const spa_free_result = spaModule.cwrap(
-          'spa_free_result',
-          null,
-          ['number']
-        );
+      const spa_free_result = spaModule.cwrap(
+        'spa_free_result',
+        null,
+        ['number']
+      );
 
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        const hour = date.getHours();
-        const minute = date.getMinutes();
-        const second = date.getSeconds();
-        const timezone = -date.getTimezoneOffset() / 60;
-        const slope = 0;
-        const azm_rotation = 0;
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const hour = date.getHours();
+      const minute = date.getMinutes();
+      const second = date.getSeconds();
+      const timezone = -date.getTimezoneOffset() / 60;
+      const slope = 0;
+      const azm_rotation = 0;
 
-        const resultPtr = spa_calculate(
-          year, month, day, hour, minute, second, timezone,
-          latitude, longitude, elevation, pressure, temperature,
-          slope, azm_rotation, refraction
-        );
+      const resultPtr = spa_calculate(
+        year, month, day, hour, minute, second, timezone,
+        latitude, longitude, elevation, pressure, temperature,
+        slope, azm_rotation, refraction
+      );
 
-        const result = {
-          zenith: spaModule.getValue(resultPtr, 'double'),
-          azimuth: spaModule.getValue(resultPtr + 8, 'double'),
-          incidence: spaModule.getValue(resultPtr + 16, 'double'),
-          sunrise: spaModule.getValue(resultPtr + 24, 'double'),
-          sunset: spaModule.getValue(resultPtr + 32, 'double'),
-          solar_noon: spaModule.getValue(resultPtr + 40, 'double'),
-          sun_transit_alt: spaModule.getValue(resultPtr + 48, 'double'),
-        };
+      const result = {
+        zenith: spaModule.getValue(resultPtr, 'double'),
+        azimuth: spaModule.getValue(resultPtr + 8, 'double'),
+        incidence: spaModule.getValue(resultPtr + 16, 'double'),
+        sunrise: spaModule.getValue(resultPtr + 24, 'double'),
+        sunset: spaModule.getValue(resultPtr + 32, 'double'),
+        solar_noon: spaModule.getValue(resultPtr + 40, 'double'),
+        sun_transit_alt: spaModule.getValue(resultPtr + 48, 'double'),
+      };
 
-        spa_free_result(resultPtr);
+      spa_free_result(resultPtr);
 
-        resolve(result);
-      },
-    });
-    if (spaModule.calledRun) {
-      spaModule.onRuntimeInitialized();
-    }
+      resolve(result);
+    };
   });
 };
